cc44bf0d460ef047427582578de3adaa7e0ce3e1
Julian zapata rugeles <juliang.@@>
2021-01-16 21:51:40 -0500

    se añadió la clase y se agregó un Qtimer para controlar el autoeliminado de la caja . (el objetivo de esta será dar bonus al personaje (balas extras cuando se acaben))

--- proyectoInformatica/bonus_municion.h
+++ proyectoInformatica/bonus_municion.h
@@ -2,12 +2,21 @@
 #define BONUS_MUNICION_H
 
 #include <QObject>
+#include <QTimer>
+#include <QDebug>
+#include <QGraphicsPixmapItem>
 
-class bonus_municion
+class bonus_municion : public QObject , public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
     bonus_municion();
+private:
+
+private slots:
+    // contador para la munición //
+    // se establecerá cuanto tiempo permanecerá la munición en pantalla antes de autoeliminarse.
+    QTimer *timer = new QTimer();
 };
 
 #endif // BONUS_MUNICION_H
