653c0dcbe595280cbd4868284077bfa8f0d9fd42
Julian zapata rugeles <juliang.@@>
2021-01-18 09:56:48 -0500

    se añadió un nuevo pixmap asteroide

--- proyectoInformatica/mundoterrestre.cpp
+++ proyectoInformatica/mundoterrestre.cpp
@@ -54,7 +54,7 @@ void mundoTerrestre::iniciarMundo()
     vista->setSceneRect(0,0,1300,600);
 
     // iniciador de timers
-    generadorAsteroides->start(15000);
+    generadorAsteroides->start(12000);
     generadorEnemigos->start(6000);
     generadorNubes->start(8000);
     generadorEnemigosGigantes->start(10000);
