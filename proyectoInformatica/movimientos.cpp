5af940d8ec36eb933c5ea339370dbd7666088133
Julian zapata rugeles <juliang.@@>
2021-01-17 19:09:24 -0500

    inclusión de la clase movimiento (hasta ahora sin implementación reciente). dicha clase contendrá la lógica del salto y otros movimientos que puedan servir a los enemigos

--- proyectoInformatica/movimientos.cpp
+++ proyectoInformatica/movimientos.cpp
@@ -1,6 +1,20 @@
  #include "movimientos.h"
 
-movimientos::movimientos()
+movimientos::movimientos(float limiteInferior_ )
 {
+    this->limite_inferior=limiteInferior_;
+    this->velocidadY= 5 * sin((90*3.1416)/180);
+    this->tiempo=0;
+}
 
+void movimientos::saltar()
+{
+    if(status_saltando==true){
+        posY=float(0+velocidadY*tiempo+(0.5*(-9.8*tiempo*tiempo)));
+        tiempo=tiempo+0.05;
+        if(posY>limite_inferior){
+            status_saltando=false;
+            tiempo=0;
+        }
+    }
 }
--- proyectoInformatica/movimientos.h
+++ proyectoInformatica/movimientos.h
@@ -1,9 +1,19 @@
 #ifndef MOVIMIENTOS_H
 #define MOVIMIENTOS_H
+#include <math.h>
 
+/*
+objetivo :
+    dotar de movimiento al personaje
+    movimiento parabolico para el salto del personaje
 
+*/
 class movimientos
 {
+private:
+    float velocidadY;
+    bool status_saltando;
+    float limite_inferior;
 public:
     movimientos();
 };
