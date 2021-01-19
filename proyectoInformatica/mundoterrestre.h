7e290a99280427697edb2d364e4652b87fed85f8
Julian zapata rugeles <juliang.@@>
2021-01-19 10:48:34 -0500

    anexo documentación de funciones principales

--- proyectoInformatica/mundoterrestre.h
+++ proyectoInformatica/mundoterrestre.h
@@ -24,8 +24,14 @@ class mundoTerrestre : public QObject , public QGraphicsRectItem
 {
     Q_OBJECT
 public:
+    // se estudia la posibilidad de sobrecargar al constructor o generar aleatoriedad en cada mundo
+    // pese a que existe cierto grado se pueden generar multiples niveles modificando los timers
+    // y el spawn del enemigos
+    // algunas condiciones de gravedad (mas o mensos) y ambientación
+
     mundoTerrestre();
     void iniciarMundo();
+
 
  private:
     QGraphicsScene * scene  = new QGraphicsScene();
@@ -33,13 +39,19 @@ public:
     QGraphicsView * vista = new QGraphicsView(scene);
     QMediaPlayer *sonido = new QMediaPlayer();
 
-    //Puntaje
+    /*
+       zona de scores y de  QGraphicsText
+       aquí estarán los objetos que mostrará información del personaje y sobre el estado de juego
+    */
     Score *puntaje = new Score;
-
-    //Tiempo de juego
     timeGame *tiempoJuego = new timeGame;
 
-    // timers
+
+
+    /* timers para el control de generación
+        NOTA : se investigará sí existe una manera más eficiente para controlar la generación.
+
+    */
     QTimer *ticks = new QTimer();
     QTimer *generadorAsteroides = new QTimer();
     QTimer *generadorEnemigos = new QTimer();
