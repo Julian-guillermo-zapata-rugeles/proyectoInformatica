ac1259088be61c8f4a1e7dc014031484a887a51d
Juan David R <juan.rendon13@udea.edu.co>
2021-01-24 18:57:17 -0500

    se implementaron dos clases nuevas, una clase llamada ship,     encargada de administrar el movimiento de las naves enemigas.     Esta clase contiene datos miembro privados que modelan el movimiento     gravitacional como son posicion x, y, radio, masa, velocidades en x, y,     y aceleraciones en x,y.     Adicionalmente tiene sus metodos get, y  metodos para realizar calculos     importantes como lo son la distancia entre dos particulas, el angulo, la     aceleracion en x, y.     Y por ultimo los metodos de asignacion de posicion, y el calulo de la     misma, y los metodos de asignacion de aceleracion y suma de la misma.
        La clase planeta  es la encargada de administrar las diferentes
        naves graficas en pantalla, esta clase hereda de QGraphicsItem de manera
        publica para redefinir las funciones vituales de boundingRect y paint,
        para pintar las naves en pantalla.
    
        Como elementos privados tiene una instancia tipo puntero de particula y
        un float para definir la escala de la nave enemiga.
    
        Como metodos tiene un get para obtener una particula, el actualizar el
        movmiento de las mismas, y la asignacion de la escala respectiva.

---
+++ proyectoInformatica/ship.h
@@ -0,0 +1,53 @@
+#ifndef SHIP_H
+#define SHIP_H
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#define G 1
+
+class Ship
+{
+public:
+    Ship(float x=0,
+         float y=0,
+         float Masa=0,
+         float Radio=0,
+         float vx=0,
+         float vy=0,
+         float ax=0,
+         float ay=0);
+
+    void asignarCuerpo(float x=0,
+                       float y=0,
+                       float Masa=0,
+                       float radio=0,
+                       float vx=0,
+                       float vy=0,
+                       float ax=0,
+                       float ay=0);
+
+    float getX() const;
+    float getY() const;
+    float getMasa() const;
+    float getRadio() const;
+    float getVx() const;
+    float getVy() const;
+    float getAx() const;
+    float getAy() const;
+
+    float calcularDistancia(const Ship p);
+    float calcularAngulo(const Ship p);
+    float calcularAcX(const Ship p);
+    float calcularAcY(const Ship p);
+    void calcularPosicion(float dt);
+    void sumarAceleracion(float Ax, float Ay);
+    void asignarAceleracion(float Ax=0, float Ay=0);
+    void asignarPosicion(float px, float py);
+
+private:
+    float x, y, masa, radio, vx, vy, ax, ay;
+};
+
+#endif // SHIP_H
