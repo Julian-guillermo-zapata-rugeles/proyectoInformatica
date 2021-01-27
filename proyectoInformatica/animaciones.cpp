f8d83daed9cbb521ecd53b50f4c235b717a7ab9f
Julian zapata rugeles <juliang.@@>
2021-01-27 11:50:36 -0500

    añadí documentación para la clase animación que nos permitirá tener mas herramientas y buenas practicas para facilitar el entendimiento del código

--- proyectoInformatica/animaciones.cpp
+++ proyectoInformatica/animaciones.cpp
@@ -2,20 +2,35 @@
 
 Animaciones::Animaciones(qreal posx, qreal posy, short tipo, bool v)
 {
+    /*
+    Constructor :
+        en esta sección se usará el tipo de animación que se presentará
+        todos los sprites presentan diferentes dimensiones
+        por ello cada elección cargará el pixmap y sus dimensiones no genéricas
+
+    */
     if(tipo == 1){
+        // Esta animación se usará para la sangre al morir un enemigo
         deadpix = new QPixmap(":/multimedia/animaciones/bloodSp.png");
         this->limite = 600;
         this->ancho = 100;
         this->alto = 100;
     }
+
     if(tipo == 2){
+        // Esta animación se usará para la explosión de asteroides o saltos
         deadpix = new QPixmap(":/multimedia/animaciones/hitSprite.png");
         this->limite = 6500;
         this->ancho = 500;
         this->alto = 500;
         this->setScale(0.5);
     }
+
     if(tipo == 3){
+        // esta animación se usará para la muerte del zombie gigante
+        // esta recibe además un booleano que representa la dirección
+        // para generar la animación muerte en el sentido correcto
+
         deadpix = new QPixmap(":/multimedia/zombieGigante/zombDie.png");
         this->limite = 2000;
         this->ancho = 153.8;
@@ -25,29 +40,45 @@ Animaciones::Animaciones(qreal posx, qreal posy, short tipo, bool v)
             setTransform(QTransform(-1, 0, 0, 1, 0, 0));
         }
     }
+
     if(tipo == 4){
+        // Este sprite se usará para las explosiones pequeñas , sin embargo se cuenta
+        // con un pixmap grande del mismo tipo de ser necesario
         deadpix = new QPixmap(":/multimedia/animaciones/minixplot.png");
         this->limite = 335;
         this->ancho = 47.85;
         this->alto = 50;
     }
+    // establecemos la ubicación del sprite (basada en la que recibimos por constructor)
+    // que representa el lugar donde muere el enemigo/asteroide/etc
     setPos(posx, posy);
 
-    tiempoVida->start(100);
+
+    tiempoVida->start(100); // el tiempo de vida será la tasa de refresco de los cuadros
     connect(tiempoVida,SIGNAL(timeout()),this,SLOT(actualizar()));
     //this->setTransformOriginPoint(this->boundingRect().center());
 }
 
+
+
 Animaciones::~Animaciones()
 {
+    /*
+     el destructor tendrá unicamente como objetivo
+     eliminar de memoria y de la escena respectivamente
+    */
     scene()->removeItem(this);
     qDebug() << "Animacion terminada "<<endl;
 }
+
+
 
 QRectF Animaciones::boundingRect() const
 {
     return QRectF(-ancho/2,-alto/2,ancho,alto);
 }
+
+
 
 void Animaciones::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
@@ -56,18 +87,30 @@ void Animaciones::paint(QPainter *painter, const QStyleOptionGraphicsItem *optio
     Q_UNUSED(widget)
 }
 
+
 void Animaciones::animar()
 {
+    /*
+    IMPORTANTE:
+        esta función usará el pixmap asignado en el constructor
+        lo recorrerá una vez y lanzará el llamado al destructor
+        la animación puede ser o no prolongada dependiendo al numero de cuadros
+        que esta pueda tener
+    */
+
     this->update();
     if(frame < limite){
         frame += ancho;
     }
     else {
+        // llamado a destruirse si sobrepasamos el limite asignado para cada sprite
         delete this;
     }
 }
 
+
 void Animaciones::actualizar()
 {
+    // slot que ejecuta la función animar
     animar();
 }
--- proyectoInformatica/animaciones.h
+++ proyectoInformatica/animaciones.h
@@ -1,6 +1,25 @@
 #ifndef ANIMACIONES_H
 #define ANIMACIONES_H
 
+/*
+El objetivo de esta clase es dotar de animaciones útiles para los objetos
+con un tiempo de vida de 1 sprite.
+Estas animaciones serán usadas por los objetos a destruir en sus destructores
+llamando así a una animación (que vivirá durante un segundo y se autodestruirá)
+eliminandose de memoria y  de la escena
+
+la sintaxis para su uso es :
+
+--> new Animacion(posicionX , posicionY , tipoAnimacion , sobrecarga )
+        -qreal  posicionX y posicionY será respectivamente la ubicación del objeto que se está eliminando
+            para presentar la animación de destrucción o muerte en esa coordenada
+
+        - short int tipo será la animación a presentar disponible (están enumeradas)
+        - bool se usará por defecto.
+
+*/
+
+
 #include <QObject>
 #include <QTimer>
 #include <QGraphicsItem>
@@ -13,26 +32,27 @@ class Animaciones : public QObject, public QGraphicsItem
 {
     Q_OBJECT
 public:
+
      Animaciones(qreal posx, qreal posy , short int tipo, bool v=false);
     ~Animaciones();
 
-signals:
-
 private:
+    // tiempo de vida será un periodo de animación.
     QTimer *tiempoVida = new QTimer;
     QPixmap *deadpix;
 
+    // se usará un ancho , alto y frame para renderizar el pixmap
     float ancho, alto, frame, limite;
     bool complete;
 
-    // QGraphicsItem interface
+    // Interfaz para el uso de QgraphicsItem
 public:
     QRectF boundingRect() const;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
-
-    void animar();
+    void animar(); // función que refrescará los cuadros de cada sprite
 
 public slots:
+    // único slot que dará la frecuencia de actualización del sprite / cambio cuadros
     void actualizar();
 };
 
